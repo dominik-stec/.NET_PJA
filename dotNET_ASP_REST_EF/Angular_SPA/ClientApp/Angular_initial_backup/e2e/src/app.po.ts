344c23bcea836b13250f6400a201fa8dc955a3b5
Dominik Stec <dccstcc@gmail.com>
2022-09-01 12:33:36 +0200

    initial Rest API and Angular project

---
+++ dotNET_ASP_REST_EF/Angular_SPA/ClientApp/Angular_initial_backup/e2e/src/app.po.ts
@@ -0,0 +1,12 @@
+﻿import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getMainHeading() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
+
